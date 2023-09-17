19ffeb92a50291356cea00bb28c7c1c303443977
Tanuj Phalke <tanujphalke5@gmail.com>
2023-09-17 12:20:07 +0530

    Intern Project

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Which is the biggest continent in the world?",
+            "Which is the longest river in the world?",
+            "Which is the largest ocean in the world?"
+
+    };
+
+    public static String choices[][]={
+            {"North America","Asia","Africa","Australia"},
+            {"Great Ganga", "Nile", "Amazon","Niger"},
+            {"Indian Ocean","Pacific Ocean","Arctic Ocean","Atlantic Ocean"}
+
+    };
+
+    public static String correctAnswers[]= {
+            "Asia",
+            "Nile",
+            "Pacific Ocean"
+    };
+}
